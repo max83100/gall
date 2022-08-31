cac49e49940dd1462abac24eaa57f858776569c0
max83100 <gguvggvhj017@gmail.com>
2022-08-31 20:06:47 +0300

    31.08

--- app/src/main/java/com/chartapp/mainboard/Mainboard_catalog.java
+++ app/src/main/java/com/chartapp/mainboard/Mainboard_catalog.java
@@ -3,7 +3,6 @@ package com.chartapp.mainboard;
 import android.annotation.SuppressLint;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -62,7 +61,7 @@ public class Mainboard_catalog extends AppCompatActivity {
         MobileAds.initialize(this, new InitializationListener() {
             @Override
             public void onInitializationCompleted() {
-                Log.d(YANDEX_MOBILE_ADS_TAG, "SDK initialized");
+                //Log.d(YANDEX_MOBILE_ADS_TAG, "SDK initialized");
             }
         });
         final AdRequest adRequest = new AdRequest.Builder().build();
